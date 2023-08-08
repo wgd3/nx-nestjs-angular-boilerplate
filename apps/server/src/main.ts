24e78cbfb8c581b78ad0cadbef561e5fab0b44fc
wgd3 <wallace@thefullstack.engineer>
2023-08-08 12:37:59 -0400

    updating server app bootstrap process with config and swagger support

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,21 +3,34 @@
  * This is only a minimal backend to get started.
  */
 
+import { QueryFailedFilter } from '@libs/server/util-common';
+import {
+  ENV_API_PREFIX,
+  ENV_ENABLE_SWAGGER,
+  ENV_SERVER_PORT,
+} from '@libs/shared/util-constants';
 import {
   ClassSerializerInterceptor,
   HttpStatus,
   Logger,
   ValidationPipe,
+  VersioningType,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { QueryFailedFilter } from '@libs/server/util-common';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableVersioning();
+  const configService = app.get(ConfigService);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
 
   const reflector = app.get(Reflector);
 
@@ -33,9 +46,15 @@ async function bootstrap() {
     })
   );
 
-  const globalPrefix = 'api';
+  if (configService.get(ENV_ENABLE_SWAGGER)) {
+    setupSwagger(app);
+  }
+
+  const globalPrefix = configService.get(ENV_API_PREFIX);
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const port = configService.get(ENV_SERVER_PORT);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
