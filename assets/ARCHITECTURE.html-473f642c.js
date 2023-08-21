36348a72a0e53fd5670b1595da21946ecc413575
wgd3 <wallace@thefullstack.engineer>
2023-08-21 13:15:59 -0400

    Updates

---
+++ assets/ARCHITECTURE.html-473f642c.js
@@ -0,0 +1,15 @@
+import{_ as e,o as i,c as s,f as n}from"./app-115987d3.js";const r={},a=n(`<h1 id="architecture" tabindex="-1"><a class="header-anchor" href="#architecture" aria-hidden="true">#</a> Architecture</h1><h2 id="repository-overview" tabindex="-1"><a class="header-anchor" href="#repository-overview" aria-hidden="true">#</a> Repository Overview</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>├── apps
+│   ├── client
+│   ├── client-e2e
+│   ├── server
+│   └── server-e2e
+├── assets
+├── dist
+│   ├── apps
+│   └── cypress
+├── docs
+├── libs
+│   ├── server
+│   └── shared
+└── tools
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="methodology" tabindex="-1"><a class="header-anchor" href="#methodology" aria-hidden="true">#</a> Methodology</h2>`,4),d=[a];function l(c,t){return i(),s("div",null,d)}const o=e(r,[["render",l],["__file","ARCHITECTURE.html.vue"]]);export{o as default};
