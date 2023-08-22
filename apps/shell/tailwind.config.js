5503969fdde4b192c99ead58b6b978016c1eb595
wgd3 <wallace@thefullstack.engineer>
2023-08-22 14:19:04 -0400

    got livereload for remotes and tailwind in shell working

--- apps/shell/tailwind.config.js
+++ apps/shell/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
+    join(__dirname, '../admin/src/**/!(*.stories|*.spec).{ts,html}'),
+    join(__dirname, '../login/src/**/!(*.stories|*.spec).{ts,html}'),
   ],
   // theme: {
   //   extend: {},
--- libs/shared/ui-tailwind/src/lib/tailwind.config.js
+++ libs/shared/ui-tailwind/src/lib/tailwind.config.js
@@ -4,28 +4,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 export default module.exports = {
   theme: {
     ...defaultTheme,
-    extend: {
-      // colors: {
-      //   primary: {
-      //     light: '#5eead4',
-      //     DEFAULT: '#14b8a6',
-      //     dark: '#0f766e',
-      //   },
-      //   secondary: {
-      //     light: '#bae6fd',
-      //     DEFAULT: '#0ea5e9',
-      //     dark: '#0369a1',
-      //   },
-      //   white: '#ffffff',
-      //   black: '#000000',
-      // },
-      // spacing: {
-      //   sm: '0.5rem',
-      //   md: '1rem',
-      //   lg: '1.5rem',
-      //   xl: '2rem',
-      // },
-    },
   },
-  plugins: [],
+  // plugins: [],
 };
