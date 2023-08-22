ae71162c8baf58762960f8b7dfa76048a7cf3825
wgd3 <wallace@thefullstack.engineer>
2023-08-22 11:33:12 -0400

    added login page with validation

--- apps/login/src/app/remote-entry/entry.component.ts
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PASSWORD_MIN_LENGTH } from '@libs/shared/util-constants';
+import { IUserLogin } from '@libs/shared/util-types';
+
+type LoginFormType = Record<keyof IUserLogin, FormControl<string>>;
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NxWelcomeComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   selector: 'nx-nestjs-angular-boilerplate-login-entry',
-  template: `<nx-nestjs-angular-boilerplate-nx-welcome></nx-nestjs-angular-boilerplate-nx-welcome>`,
+  templateUrl: `entry.component.html`,
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  loginForm = new FormGroup<LoginFormType>({
+    email: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur',
+    }),
+    password: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(PASSWORD_MIN_LENGTH),
+      ],
+      updateOn: 'blur',
+    }),
+  });
+
+  get fEmail(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get fPassword(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  // TODO add login logic
+  login() {
+    if (this.loginForm.dirty && this.loginForm.valid) {
+      console.log(`Logged In!`);
+    } else {
+      alert(`Form is invalid!`);
+    }
+  }
+}
---
+++ libs/shared/ui-tailwind/src/lib/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+export default module.exports = {
+  theme: {
+    ...defaultTheme,
+    extend: {
+      // colors: {
+      //   primary: {
+      //     light: '#5eead4',
+      //     DEFAULT: '#14b8a6',
+      //     dark: '#0f766e',
+      //   },
+      //   secondary: {
+      //     light: '#bae6fd',
+      //     DEFAULT: '#0ea5e9',
+      //     dark: '#0369a1',
+      //   },
+      //   white: '#ffffff',
+      //   black: '#000000',
+      // },
+      // spacing: {
+      //   sm: '0.5rem',
+      //   md: '1rem',
+      //   lg: '1.5rem',
+      //   xl: '2rem',
+      // },
+    },
+  },
+  plugins: [],
+};
